07bd7642d6323b7c1fa25f284407c5928db70d19
xinyu <fangjingyu0502@163.com>
2019-11-29 09:33:05 +0800

    更新配置地址

--- assets/mapdata/js/basic_config.js
+++ assets/mapdata/js/basic_config.js
@@ -39,9 +39,10 @@ function serveRequest(type,url,params_arguments,callBack){
 //服务接口配置
 var service_config = {
     data_server_url : "http://47.96.71.167:8901/",
-    // file_server_url : "http://47.96.71.167:8089/web/assets/mapdata/json/",
-    file_server_url : "http://127.0.0.1:5500/assets/mapdata/json/",
-    icon_url: "http://127.0.0.1:5500/assets/mapdata/images/",
+    file_server_url : "http://47.96.71.167:8089/web/assets/mapdata/json/",
+    // file_server_url : "http://127.0.0.1:5500/assets/mapdata/json/",
+    icon_url: "http://47.96.71.167:8089/web/assets/mapdata/images/",
+    // icon_url: "http://127.0.0.1:5500/assets/mapdata/images/",
     server_img_url: "http://47.96.71.167:8089/",
 }
 //颜色配置
