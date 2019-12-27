4a07701ac4977249bf5b5f9c2178b64bf47bdd33
xinyu <fangjingyu0502@163.com>
2019-12-27 14:44:45 +0800

    更细地址

--- assets/mapdata/js/basic_config.js
+++ assets/mapdata/js/basic_config.js
@@ -39,9 +39,9 @@ function serveRequest(type,url,params_arguments,callBack){
 //服务接口配置
 var service_config = {
     data_server_url : "http://114.64.228.103:8902",
-    file_server_url : "http://114.64.228.103:8089/web/assets/mapdata/json/",
+    file_server_url : "http://peking.caupdcloud.com/dongcheng/assets/mapdata/json/",
     // file_server_url : "http://127.0.0.1:5500/assets/mapdata/json/",
-    icon_url: "http://114.64.228.103:8089/web/assets/mapdata/images/",
+    icon_url: "http://peking.caupdcloud.com/dongcheng/assets/mapdata/images/",
     // icon_url: "http://127.0.0.1:5500/assets/mapdata/images/",
     server_img_url: "http://114.64.228.103:8089/",
 }
